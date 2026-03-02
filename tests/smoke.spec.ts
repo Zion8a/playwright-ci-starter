4b61f9d3dd6c24dc3f710c67184efbe60c70047f
Zion8a <johan@bergochhav.nu>
2026-03-02 12:50:04 +0100

    Revert failing test

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('homepage has correct title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Definitely Not The Title/);
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link works', async ({ page }) => {
